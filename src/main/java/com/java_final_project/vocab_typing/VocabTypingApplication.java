183f59f97253a373d453042d5941e824578be050
Jesse <jessie97013@gmail.com>
2025-05-08 23:55:13 +0800

    Initial Spring Boot project structure

---
+++ src/main/java/com/java_final_project/vocab_typing/VocabTypingApplication.java
@@ -0,0 +1,13 @@
+package com.java_final_project.vocab_typing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabTypingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabTypingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java_final_project/vocab_typing/VocabTypingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_final_project.vocab_typing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabTypingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
