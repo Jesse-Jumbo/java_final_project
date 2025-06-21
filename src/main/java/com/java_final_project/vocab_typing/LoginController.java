94c138f853490e78318d5da0e6c955e3f38c4798
mihsia0506 <113426684+mihsia0506@users.noreply.github.com>
2025-06-21 20:14:32 +0800

    Add #

--- src/main/java/com/java_final_project/vocab_typing/LoginController.java
+++ src/main/java/com/java_final_project/vocab_typing/LoginController.java
@@ -1,6 +1,6 @@
 package com.java_final_project.vocab_typing;
 
-Dimport jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
